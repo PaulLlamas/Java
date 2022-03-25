fc90edaf5f1e691640e3a6ead81839a3d6ecbd78
Paul Llamas <paul.llamas@my.simpson.edu>
2022-03-25 09:47:09 -0500

    Notes

---
+++ src/tryStacks/ThirdRTStack.java
@@ -0,0 +1,14 @@
+package tryStacks;
+
+public class ThirdRTStack {
+    public static int g(int x){
+        return 1;
+    }
+    public static void f(int a, int b){
+
+    }
+
+    public static void main(String[] args) {
+        f(g(2), 5);
+    }
+}
