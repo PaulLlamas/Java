0814bb5d1b5e4be4bb5290be900f6d57d0716cac
Paul Llamas <paul.llamas@my.simpson.edu>
2022-04-11 10:06:48 -0500

    notes

---
+++ src/multidimensionalArrays/PascalTriangle.java
@@ -0,0 +1,26 @@
+package multidimensionalArrays;
+
+public class PascalTriangle {
+
+    public static int[][] pascalTriangle(int numberOfRows){
+        int[][] pascal = new int[numberOfRows + 1][];
+        pascal[0] = new int[1];
+        pascal[0][0] = 1;
+        pascal[1] = new int[2];
+        pascal[1][0] = 1;
+        pascal[1][1] = 1;
+        for(int framework = 2; framework <= numberOfRows; framework++){
+            pascal[framework] = new int[framework + 1];
+            pascal[framework][0] = 1;
+            pascal[framework][framework] = 1;
+            for(int middle = 1; middle < framework; middle++){
+                pascal[framework][middle] = pascal[framework - 1][middle - 1] + pascal[framework - 1][middle];
+            }
+        }
+        return pascal;
+    }
+
+    public static void main(String[] args) {
+        PrintUtils.printIntMatrix(pascalTriangle(5));
+    }
+}
